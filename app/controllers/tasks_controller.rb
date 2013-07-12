546fe27f2e57ce5586e0bcb79e2efb8ad132773a
pjgsonido12 <psonido@nazarene.org>
2013-07-13 04:32:55 +0800

    Added def codes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,20 +21,39 @@ class TasksController < ApplicationController
     
     
     def show
-      @task = Task.find(params[:id])
-      @comment = @task.comments.build
-      @comments = Comment.where(:task_id => @task.id)
-      @people = Person.is_active
-      @permissions = Permission.where(:project_id => current_project.id)
+      respond_to do |format|
+        format.html {
+        @task = Task.find(params[:id])
+        @comment = @task.comments.build
+        @comments = Comment.where(:task_id => @task.id)
+        @people = Person.is_active
+        @permissions = Permission.where(:project_id => current_project.id)
       
-      if @task.task_status.name == 'Assigned' || @task.task_status.name == 'Re-Assigned' || @task.task_status.name == 'Re-Open'
-        @task_statuses = TaskStatus.task_assigned
-      elsif @task.task_status.name == 'Accepted'
-        @task_statuses = TaskStatus.task_accepted
-      elsif @task.task_status.name == 'Resolved'
-        @task_statuses = TaskStatus.task_resolved         
-      else
-        @task_statuses = TaskStatus.task_done     
+        if @task.task_status.name == 'Assigned' || @task.task_status.name == 'Re-Assigned' || @task.task_status.name == 'Re-Open'
+          @task_statuses = TaskStatus.task_assigned
+        elsif @task.task_status.name == 'Accepted'
+          @task_statuses = TaskStatus.task_accepted
+        elsif @task.task_status.name == 'Resolved'
+          @task_statuses = TaskStatus.task_resolved         
+        else
+          @task_statuses = TaskStatus.task_done     
+        end
+        } # show.html.erb
+        
+        format.pdf  {
+          task_number = $1
+          pdfname = task_number.to_s + "_" + "Standard_Profile.pdf"
+          prawn_attributes = { :top_margin => 40, :left_margin => 25, :page_layout => :landscape }
+          @task = Task.find_by_task_number(task_number)
+          profile = StandardProfile.new(@task,prawn_attributes)
+          
+          if @task
+            send_data profile.to_pdf, :filename => pdfname, :type => "application/pdf", :disposition => 'inline'
+          else
+            send_data IO.read(File.join(Rails.root, 'public', '404.html')), :status => "404 Not Found", :disposition => 'inline'
+          end
+        }
+        
       end
     end
     
@@ -59,9 +78,10 @@ class TasksController < ApplicationController
         redirect_to :back
       else  
         @people = Person.find(params[:people])
+        project = Project.where(:id => current_project.id).last        
        if @task.save
          for person in @people
-           UserMailer.registration_confirmation(person).deliver
+           UserMailer.created_task(person, project, @task).deliver
          end
          flash[:notice] = "You have successfully created new task."
          redirect_to project_tasks_url
@@ -73,21 +93,12 @@ class TasksController < ApplicationController
     
     def edit
        @task = Task.find(params[:id])
-       @people = Person.is_active
     end
 
-    def update
+    def update_task
       @task = Task.find(params[:id])
-      if params[:people].nil?
-        flash[:notice] = "Please check atleast one person."
-        redirect_to :back
-      else  
-        @people = Person.find(params[:people])
         respond_to do |format|
          if @task.update_attributes(params[:task])
-           for person in @people
-             UserMailer.registration_confirmation(person).deliver
-           end
            flash[:notice] = "You have successfully updated task information."
            format.html { redirect_to project_task_url }
            format.xml  { head :ok }
@@ -97,7 +108,30 @@ class TasksController < ApplicationController
          end
        end
      end
-     end
+     
+     def update
+       @task = Task.find(params[:id])
+       if params[:people].nil? 
+         flash[:notice] = "Please check atleast one person."
+         redirect_to :back
+       else  
+         @people = Person.find(params[:people])
+         project = Project.where(:id => current_project.id).last  
+         respond_to do |format|
+          if @task.update_attributes(params[:task])
+            for person in @people
+              UserMailer.updated_task(person, project, @task).deliver
+            end
+            flash[:notice] = "You have successfully updated task information."
+            format.html { redirect_to project_task_url }
+            format.xml  { head :ok }
+          else
+            format.html { render :edit }
+            format.xml  { render :xml => @task.errors, :status => :unprocessable_entity }
+          end
+        end
+      end
+      end
 
      def destroy
        @task = Task.find(params[:id])
@@ -114,5 +148,15 @@ class TasksController < ApplicationController
 
        redirect_to(project_tasks_url)
      end
+     
+     def remove
+       task = Task.find(params[:task_id])
+       @comment = Comment.find(params[:id])
+       @comment.is_active = false
+       @comment.save
+
+       flash[:notice] = "You have successfully removed your comment."
+       redirect_to project_task_url(current_project,task)
+     end    
      
 end
