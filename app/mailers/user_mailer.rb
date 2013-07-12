71c98446a857ed5f1e39aa90f77d3904f2101757
pjgsonido12 <psonido@nazarene.org>
2013-07-13 04:35:57 +0800

    Added mailer code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,15 @@
 class UserMailer < ActionMailer::Base
-  def registration_confirmation(person)
+  def created_task(person, project, task)
     @person = person
-    mail(:to => person.email, :from => "Ticketing System <no-reply@ncm.org>", :subject => "Task Assignment")
+    @project = project
+    @task = task
+    mail(:to => person.email, :from => "Ticketing System <no-reply@ncm.org>", :subject => "New Task Assignment")
+  end
+  
+  def updated_task(person, project, task)
+    @person = person
+    @project = project
+    @task = task
+    mail(:to => person.email, :from => "Ticketing System <no-reply@ncm.org>", :subject => "Task Assignment Update")
   end
 end
