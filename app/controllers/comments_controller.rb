4a7f3be856f3c9426605eeda24d2363193ce6a03
pjgsonido12 <psonido@nazarene.org>
2014-02-13 04:21:26 +0800

    added comment mailer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,18 @@ class CommentsController < ApplicationController
   # POST /comments
   def create
     task = Task.find(params[:task_id])
-    @comment = task.comments.build(params[:comment])
-    
+    @comment = task.comments.build(params[:comment])            
+    @task = task
+    @people = Person.where(['id = ? OR id = ? OR id = ?', current_user.id, 30, task.assigned_to])
+    @project = Project.where(:id => current_project.id).last  
+
     if @comment.save
+      for person in @people
+        UserMailer.comment_task(person, @project, @task, @comment).deliver
+       end
       flash[:notice] = "You have successfully posted your comment."
-      redirect_to project_task_url(current_project,task)
-    else
-         
-    end
+       redirect_to project_task_url(current_project,task)
+     end 
   end
   
 end
