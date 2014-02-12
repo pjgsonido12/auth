d4b9bb190d76b6323221709f793d333b73703b6d
pjgsonido12 <psonido@nazarene.org>
2014-02-13 04:21:45 +0800

    added comment mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,4 +12,12 @@ class UserMailer < ActionMailer::Base
     @task = task
     mail(:to => person.email, :from => "Chronos <no-reply@ncm.org>", :subject => project.name + ": Task No. "  + task.task_number.to_s)
   end
+  
+  def comment_task(person, project, task, comment)
+    @person = person
+    @project = project
+    @task = task
+    @comment = comment
+    mail(:to => person.email, :from => "Chronos <no-reply@ncm.org>", :subject => project.name + ": Task No. "  + task.task_number.to_s, :content_type => "text/html")
+  end
 end
