293bdc68df86d53196da12b2ff9ac0cf6f8aeb63
pjgsonido12 <psonido@nazarene.org>
2014-04-03 03:19:59 +0800

    modified permissions

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -3,18 +3,23 @@ class PermissionsController < ApplicationController
   before_filter :authenticate_project!
   def index
     @permission = Permission.new
-    @people = Person.is_active
-    @project_people = current_project.people.is_active
-    
-    @people = Permission.where(:project_id => current_project.id)
-    #@people = Person.find_by_sql("select * from people where id in ( select person_id from permissions ) and is_active = '1' ")
-        
+    @project_people = current_project.people.is_active    
+    @people = Permission.where(:project_id => current_project.id)  
+    @add_people = Person.find_by_sql("SELECT * from people where id not in (SELECT person_id from permissions where permissions.project_id = #{current_project.id}) and people.is_active = '1' ")     
     @roles = Role.all
     @admin_roles = Permission.where(:project_id => current_project.id, :role_id => '1')
     @user_roles = Permission.where(:project_id => current_project.id, :role_id => '2')
     @spec_roles = Permission.where(:project_id => current_project.id, :role_id => '3')
   end
 
+  def create
+    @permission = Permission.new(params[:permission])
+    if @permission.save
+      redirect_to project_permissions_url
+    else
+      render "new"
+    end
+  end
   
   def update_role
     @permission = Permission.where(:project_id => params[:project_id], :person_id => params[:person_id])
